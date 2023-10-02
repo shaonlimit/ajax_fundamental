0c3b6664862379e737f257648eabd75bf0d74427
shaonlimit <shasanexp@gmail.com>
2023-10-02 10:40:14 +0600

    lesson_2

--- lesson_2/script.js
+++ lesson_2/script.js
@@ -1,12 +1,12 @@
-const loadData = () => {
+const loadData = (callBackFunction) => {
 
     //create a request
     const xhr = new XMLHttpRequest();
 
     //what we need to do when response arrives
-    xhr.onload = function () {
-       document.getElementById('ajax').innerHTML = this.responseText;
-    }
+    xhr.onload = function (){
+        callBackFunction(xhr);
+    };
 
     //preparation stage
     xhr.open('GET','./data/data.txt');
@@ -19,4 +19,13 @@ const loadData = () => {
 
     //abort request
     // xhr.abort();
+}
+
+
+const myCallBack_1 = (xhr) => {
+    document.getElementById('demo_1').innerHTML = xhr.responseText;
+}
+
+const myCallBack_2 = (xhr) => {
+    document.getElementById('demo_2').innerHTML = xhr.responseText;
 }
